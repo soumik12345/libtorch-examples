2179fb24dd17c72da1ad0982407fdf11f50f1181
soumik12345 <19soumik.rakshit96@gmail.com>
2021-07-01 12:35:40 +0530

    Updated Convnet Example

--- libtorch-examples/convnet/main.cpp
+++ libtorch-examples/convnet/main.cpp
@@ -3,6 +3,8 @@
 int main() {
     ConvNetClassifier classifier;
     classifier.compile(10, 0.001);
-    classifier.train("../../../data/mnist", 100, 5, "../../checkpoints");
+    classifier.train(
+            "../../../data/mnist", 100,
+            5, "../../checkpoints");
     classifier.evaluate("../../../data/mnist", 100);
 }
